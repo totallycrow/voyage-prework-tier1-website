6577dd66597ae279d68aa5ebfa1ab1054a612ded
Adrian Gajda <adrian.gajda@hotmail.com>
2019-08-26 21:44:03 +0100

    Fixed 'external' tab to link to an external page, fixed console errors when clicking on 'external' tab

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
 // Navbar toggle
 
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
-navBarToggle.addEventListener('click', function () {
-    
-    mainNav.classList.toggle('active');
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
 });
 
 // Smooth-scroll
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
     });
+  });
 });
 
 // Scroll to top button
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
